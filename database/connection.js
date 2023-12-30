9a283efc313ef8f232badc5e72f9870f7d6772fa
Camilo Posik <135071911+ElKittyy@users.noreply.github.com>
2023-12-30 18:54:59 -0300

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'imprenta'
+    host: 'bio0y98163lor2sxfrb4-mysql.services.clever-cloud.com',
+    user: 'uxlvnw0zig1qdllc',
+    password: 'uxlvnw0zig1qdllc',
+    database: 'bio0y98163lor2sxfrb4'
 });
 
 export {connection};
